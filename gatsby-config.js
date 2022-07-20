ea2e2aebda6b78c4cd783c5ea98d50efc12f3427
MagicMart <martcan27@gmail.com>
2022-07-20 20:17:50 +0000

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `A View on Access`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Tim Calvert`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
