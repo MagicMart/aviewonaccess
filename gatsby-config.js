1f4c441c2010bd9ae0dc7b268a8b53fc3c364619
Martin Tudor <martcan27@gmail.com>
2022-08-02 19:52:33 +0100

    update images

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/creative-concepts.png`, // This path is relative to the root of the site.
+        icon: `src/images/creative-concepts.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
