e13e7f7b693cac881ae9e6b3650f35cb58997c68
MagicMart <martcan27@gmail.com>
2024-01-26 21:02:20 +0000

    add spotify iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,17 +14,49 @@ const Home = ({ data, location }) => {
       <Bio />
       <article>
         <p>
-          <span className="bold">A View on Access</span> was created in 2015 in conjunction with the Audio Description Association and from 2016 to June 2022 had a home on RNIB Connect. It was developed to create a better sense of access and inclusion in the arts and entertainment sector, that includes theatre, cinema, museums and galleries, attractions, conventions, events and sport for those blind and partially sighted - but also to provide an audio platform for other disabilities.
+          <span className="bold">A View on Access</span> was created in 2015 in
+          conjunction with the Audio Description Association and from 2016 to
+          June 2022 had a home on RNIB Connect. It was developed to create a
+          better sense of access and inclusion in the arts and entertainment
+          sector, that includes theatre, cinema, museums and galleries,
+          attractions, conventions, events and sport for those blind and
+          partially sighted - but also to provide an audio platform for other
+          disabilities.
         </p>
         <p>
-          Over the last 7 Years, <span className="bold">A View on Access</span> has gone from strength to strength, developing relationships with relevant companies, charities, venues and organisations in order to create better awareness of access and audio description.
+          Over the last 7 Years, <span className="bold">A View on Access</span>{" "}
+          has gone from strength to strength, developing relationships with
+          relevant companies, charities, venues and organisations in order to
+          create better awareness of access and audio description.
         </p>
         <p>
-          The project operates from a positive viewpoint to encourage communities to engage with the arts and events but also helps to improve services by feeding back any issues or problems and offering advice on how to fix them.
+          The project operates from a positive viewpoint to encourage
+          communities to engage with the arts and events but also helps to
+          improve services by feeding back any issues or problems and offering
+          advice on how to fix them.
         </p>
         <p>
-          <span className="bold">A View on Access</span> is produced and presented by <em>Tim Calvert</em> of <em>Calvert Creative Concepts</em> in collaboration with the Audio Description Association: Showcasing the best of entertainment, arts and culture around the UK from an accessible point of view; highlighting the audio description available and how inclusive content and venues are. This is presented in the form of an audio podcast feature. <a href="https://audioboom.com/channels/5082711/">Listen to Tim on <span className="bold">Audioboom</span></a>
+          <span className="bold">A View on Access</span> is produced and
+          presented by <em>Tim Calvert</em> of{" "}
+          <em>Calvert Creative Concepts</em> in collaboration with the Audio
+          Description Association: Showcasing the best of entertainment, arts
+          and culture around the UK from an accessible point of view;
+          highlighting the audio description available and how inclusive content
+          and venues are. This is presented in the form of an audio podcast
+          feature.{" "}
+          <a href="https://audioboom.com/channels/5082711/">
+            Listen to Tim on <span className="bold">Audioboom</span>
+          </a>
         </p>
+        <iframe
+          src="https://open.spotify.com/embed/show/2Nr64BUqOcfj383HtjFp6M?utm_source=generator&theme=0&t=0"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
         <p>For information, to make a suggestion or to get involved email:</p>
         <address>
           <a href="mailto:aviewonaccess@gmail.com">aviewonaccess@gmail.com</a>
@@ -33,13 +65,21 @@ const Home = ({ data, location }) => {
       <article>
         <h2>About Tim Calvert</h2>
         <p>
-          <span className="bold">A View on Access</span> is presented and produced by <em>Tim Calvert</em> of <em>Calvert Creative Concepts</em>.
+          <span className="bold">A View on Access</span> is presented and
+          produced by <em>Tim Calvert</em> of <em>Calvert Creative Concepts</em>
+          .
         </p>
         <p>
-          Tim is a skilled consultant in access, inclusion and audio description, and is a highly qualified audio producer with a BA honours degree from Staffordshire University.
+          Tim is a skilled consultant in access, inclusion and audio
+          description, and is a highly qualified audio producer with a BA
+          honours degree from Staffordshire University.
         </p>
         <p>
-          Tim is Vice chair of the Audio description association, and sits on the UK Cinema Association working disability group and has worked with many companies and organisations to improve accessibility including ATG, Trafalgar Entertainment, Hampstead Theatre, Appetite Stoke, and The Independent Cinema Office.
+          Tim is Vice chair of the Audio description association, and sits on
+          the UK Cinema Association working disability group and has worked with
+          many companies and organisations to improve accessibility including
+          ATG, Trafalgar Entertainment, Hampstead Theatre, Appetite Stoke, and
+          The Independent Cinema Office.
         </p>
       </article>
     </Layout>
@@ -56,4 +96,4 @@ export const pageQuery = graphql`
       }
     }
   }
-  `
+`
